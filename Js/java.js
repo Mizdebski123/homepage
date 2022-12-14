d5fb94fe7a4051f9f4155c7648c974c95201af33
Marcin Izdebski <mizdebski123@gmail.com>
2022-12-14 22:42:52 +0100

    Initial commit

---
+++ Js/java.js
@@ -0,0 +1,10 @@
+console.log ("Siema!")
+let changeBackgroundButton = document.querySelector(".section__button--background");
+let body = document.querySelector(".body");
+let nextColorName = document.querySelector(".nextColorName")
+
+changeBackgroundButton.addEventListener("click", ()=> {
+    body.classList.toggle("greyBackground");
+    nextColorName.innerText = body.classList.contains("greyBackground") ? "białe" : "szare";
+
+});
